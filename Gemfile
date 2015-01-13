d9e5b049d37ae2b98c4b990fc3a8f2308564dafd
India Dearlove <india_dearlove@yahoo.co.uk>
2015-01-13 13:48:26 +0000

    Install cleaner

--- Gemfile
+++ Gemfile
@@ -1,2 +1,9 @@
 source 'https://rubygems.org'
 gem 'sinatra'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'database_cleaner'
+
+group :test do
+  gem 'rspec'
+end
---
+++ spec/link_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Link do
+
+  context 'Demonstration of how datamapper works' do
+
+    it 'should be created and then retrieved from the db' do
+      Link.destroy
+      expect(Link.count).to eq(0)
+      Link.create(title: 'Makers Academy',
+        url: 'https://www.makersacademy.com/')
+      expect(Link.count).to eq(1)
+      link=Link.first
+      expect(link.url).to eq('https://www.makersacademy.com/')
+      expect(link.title).to eq('Makers Academy')
+      Link.destroy
+      expect(Link.count).to eq(0)
+    end
+
+  end
+
+end
