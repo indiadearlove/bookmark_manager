b8802022098211fa02a9d9e31d4b593ade89ac50
India Dearlove <india_dearlove@yahoo.co.uk>
2015-01-14 10:02:20 +0000

    working links page

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -10,4 +10,5 @@ feature "User browses the list of links" do
     visit '/'
     expect(page).to have_content("Makers Academy")
   end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV['RACK_ENV'] = 'test'
 require './server'
 require 'database_cleaner'
 require 'capybara/rspec'
+require 'capybara'
 
 Capybara.app = BookmarkManager
 
