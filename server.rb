5b5aa550177f5b8ac1d98a06a67863af10c33cf0
India Dearlove <india_dearlove@yahoo.co.uk>
2015-01-14 11:54:30 +0000

    add tags

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'data_mapper'
 require 'sinatra/base'
+require 'tag'
 
 env = ENV['RACK_ENV'] || 'development'
 
@@ -21,8 +22,12 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     url = params["url"]
     title = params["title"]
-    Link.create(:url => url, :title => title)
+    tags = params["tags"].split(" ").map do |tag|
+      Tag.first_or_create(:text => tag)
+    end
+    Link.create(:url => url, :title => title, :tags => tags)
     redirect to('/')
   end
+
 
 end
